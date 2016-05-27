7530aa39b6c5ec4178b6af2c9d2b5a7f50543885
Stephanie <wondafury@gmail.com>
2016-05-26 18:11:00 -0700

    initial pseudocode file

---
+++ final_project_draft.py
@@ -0,0 +1,26 @@
+# Main menu
+# 	1 - Set up account for new user
+# 	2 - Enter activity and steps for returning user
+# 	3 - See metrics for returning user
+
+# Setting up account
+ # 	Prompt user for name 
+ # 	Prompt user for desired daily target number of steps
+
+# Entering activities and converting to steps
+# 	Prompt user for name to access account
+# 	Prompt user for type of activity that was peformed
+# 	Prompt user for number of minutes (duration) of activity
+# 	Print the converted number of steps from the activity
+# 	Subtract completed steps from daily target and calculate remaining number of steps needed
+# 	If daily target is acheived, print message to congratulate user
+# 	Else print number of remaining steps needed to achieve daily target and motivational message
+# 	Store commonly used activities/durations when user often performs same activity on a regular basis
+#	Prompt user to exit program when done
+
+# Checking metrics
+# 	Prompt user for name to access account
+# 	Prompt user for which duration of metrics they want to see (weekly, monthly)
+# 	Print average daily step count from the chosen duration
+# 	Prompt user to exit program when done
+
